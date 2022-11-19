d205cec95bb38896609f9c523fcd418d6f43057a
João Pedro Luz <101302893+JoaoLuzWork@users.noreply.github.com>
2022-11-19 16:11:15 -0300

    commit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__ . '/vendor/autoload.php';
-    
+    require_once './dispositivo.php';
+
     $loader = new \Twig\Loader\FilesystemLoader('./views');
     $twig = new \Twig\Environment($loader);
 
@@ -12,11 +13,7 @@
 
     $dados = array(
 
-        "title" => "Home",
-        "Matriz2x" => "Calcular matriz 2x2:",
-        "Matriz3X" => "Calcular matriz 3x3:",
-        "escolha" => "Escolha qual tamanho de matriz deseja calcular:"
-        
+        "dispositivo" => $dispositivo,
     );
 
     $template = $twig->load('index.twig');
